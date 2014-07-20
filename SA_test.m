bc3ad381ce891be17e5f96435b275d4da7646a20
Rui Zhao <oahziur@gmail.com>
2014-07-20 16:05:39 -0400

    Add readme for Simulated Annealing

--- SA_test.m
+++ SA_test.m
@@ -1,13 +1,28 @@
 function [] = SA_test()
+  SA_samll_test();
+end
+
+function [] = SA_samll_test()
   m = 3;
   n = 6;
   S = ones(1, n);
   J = [2, 3, 4, 6, 2, 2];
-  [costs, bestSol] = SA(S, J, m, n, 2, 30, 0.01, 0.85, @cost, @gen_neighbor);
+  sT = findStartTemp(J, m);
+  [costs, bestSol] = SA(S, J, m, n, 2, sT, 0.01, 0.85, @cost, @gen_neighbor);
   if cost(bestSol, J, 3, 6) == 7
     disp("small test PASSED!");
   else
     disp("small test FAILED!");
   end
   plot(costs);
+end
+
+function [sT] = findStartTemp(jobs, m)
+  sT = 0;
+  maxChange = max(jobs);
+  avgExeTime = sum(jobs) / m;
+  if (avgExeTime > maxChange)
+    maxChange = avgExeTime;
+  end
+  sT = -1* maxChange / log(0.85);
 end
