c283d6dc4f2651f651e52af66770e813af87be9f
Rui Zhao <oahziur@gmail.com>
2014-07-23 11:12:49 -0400

    Update phermone scheme

--- ACO.m
+++ ACO.m
@@ -5,7 +5,9 @@ function [costs, bestSol] = ACO(jobs, m, n, ants, iterations, costFunc)
   costs = [];
   bestSol = ones(1, n);
   bestSolCost = costFunc(bestSol, jobs, m, n);
-  phermone = ones(n, n*n); % track phermone value on each path.
+
+  phermone = ones(n, m*m); % track phermone value on each path.
+
   for i = 1:iterations
     for a = 1:ants;
 
@@ -14,8 +16,23 @@ function [costs, bestSol] = ACO(jobs, m, n, ants, iterations, costFunc)
       for l = 1:n % going down n layers in total
 
         if l == 1 % layer 1 to n mapping
+          phermone_to_next_level = [];
+          for j = 1:m
+            phermone_to_next_level(j) = phermone(1, j);
+          end
 
         else % rest layers are n to n mapping
+          last_node = ant_path(l-1);
+
+          phermone_row = l;
+          phermone_col_s = (last_node-1) * m + 1;
+          phermone_col_f = last_node * m;
+          phermone_to_next_level = [];
+          temp_index = 1;
+
+          for j = phermone_col_s:phermone_col_f
+            phermone_to_next_level(temp_index) = phermone(phermone_row, j);
+          end
 
         end
 
