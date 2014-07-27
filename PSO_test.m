34cdd2d5a4fa88283639ceef3697449ecc8656a2
Rui Zhao <oahziur@gmail.com>
2014-07-27 18:01:07 -0400

    Adjust PSO and SA tests

--- PSO_test.m
+++ PSO_test.m
@@ -1,27 +1,26 @@
 function [] = PSO_test()
-  SA_samll_test();
-  SA_16t6m_test();
-  SA_17t5m_test();
+  PSO_samll_test(150);
+  PSO_16t6m_test(500);
+  PSO_17t5m_test(500);
 end
 
-function [] = SA_samll_test()
+function [costs] = PSO_samll_test(iterations)
   m = 3;
   n = 6;
   J = [2, 3, 4, 6, 2, 2];
-  [costs, bestSol] = PSO_lbest(J, m, n, 1500, 150, @cost);
+  [costs, bestSol] = PSO_lbest(J, m, n, 1500, iterations, @cost);
   if cost(bestSol, J, 3, 6) == 7
     disp('small test PASSED!');
   else
     disp('small test FAILED!');
   end
-  plot(costs);
 end
 
-function [] = SA_16t6m_test()
+function [costs] = PSO_16t6m_test(iterations)
   m = 6;
   n = 16;
   J = [58,72,79,43,16,37,7,74,97,44,39,80,65,65,39,96];
-  [costs, bestSol] = PSO_lbest(J, m, n, 500, 500, @cost);
+  [costs, bestSol] = PSO_lbest(J, m, n, 500, iterations, @cost);
   bestSolCost = cost(bestSol, J, 6, 16);
   if  bestSolCost == 154
     disp('16t6m test PASSED!');
@@ -29,14 +28,13 @@ function [] = SA_16t6m_test()
     disp('16t6m test FAILED!');
     disp(bestSolCost);
   end
-  plot(costs);
 end
 
-function [] = SA_17t5m_test()
+function [costs] = PSO_17t5m_test(iterations)
   m = 5;
   n = 17;
   J = [50,22,13,64,43,70,53,55,95,83,54,56,60,77,28,24,25];
-  [costs, bestSol] = PSO_lbest(J, m, n, 500, 500, @cost);
+  [costs, bestSol] = PSO_lbest(J, m, n, 500, iterations, @cost);
   bestSolCost = cost(bestSol, J, 6, 16);
   if  bestSolCost == 176
     disp('17t5m test PASSED!');
@@ -44,5 +42,4 @@ function [] = SA_17t5m_test()
     disp('17t5m test FAILED!');
     disp(bestSolCost);
   end
-  plot(costs);
 end
--- SA_test.m
+++ SA_test.m
@@ -1,30 +1,36 @@
 function [] = SA_test()
-  SA_samll_test();
-  SA_16t6m_test();
+  SA_samll_test(100);
+  SA_16t6m_test(10000);
+  SA_17t5m_test(10000);
 end
 
-function [] = SA_samll_test()
+function [costs] = SA_samll_test(iterations)
   m = 3;
   n = 6;
   S = initSolution(m,n);
   J = [2, 3, 4, 6, 2, 2];
   sT = findStartTemp(J, m);
-  [costs, bestSol] = SA(S, J, m, n, 2, sT, 0.01, 0.85, @cost, @gen_neighbor);
+  alpha = 0.85;
+  iterationsAtTemp = 2;
+  fT = sT*(alpha^(iterations/iterationsAtTemp));
+  [costs, bestSol] = SA(S, J, m, n, iterationsAtTemp, sT, fT, alpha, @cost, @gen_neighbor);
   if cost(bestSol, J, 3, 6) == 7
     disp('small test PASSED!');
   else
     disp('small test FAILED!');
   end
-  plot(costs);
 end
 
-function [] = SA_16t6m_test()
+function [costs] = SA_16t6m_test(iterations)
   m = 6;
   n = 16;
   S = initSolution(m,n);
   J = [58,72,79,43,16,37,7,74,97,44,39,80,65,65,39,96];
   sT = findStartTemp(J, m);
-  [costs, bestSol] = SA(S, J, m, n, 55, sT, 0.000001, 0.85, @cost, @gen_neighbor);
+  alpha = 0.85;
+  iterationsAtTemp = 55;
+  fT = sT*(alpha^(iterations/iterationsAtTemp));
+  [costs, bestSol] = SA(S, J, m, n, iterationsAtTemp, sT, fT, alpha, @cost, @gen_neighbor);
   bestSolCost = cost(bestSol, J, 6, 16);
   if  bestSolCost == 154
     disp('16t6m test PASSED!');
@@ -32,7 +38,25 @@ function [] = SA_16t6m_test()
     disp('16t6m test FAILED!');
     disp(bestSolCost);
   end
-  plot(costs);
+end
+
+function [costs] =  SA_17t5m_test(iterations)
+  m = 5;
+  n = 17;
+  S = initSolution(m,n);
+  J = [50,22,13,64,43,70,53,55,95,83,54,56,60,77,28,24,25];
+  sT = findStartTemp(J, m);
+  alpha = 0.85;
+  iterationsAtTemp = 55;
+  fT = sT*(alpha^(iterations/iterationsAtTemp));
+  [costs, bestSol] = SA(S, J, m, n, iterationsAtTemp, sT, fT, alpha, @cost, @gen_neighbor);
+  bestSolCost = cost(bestSol, J, 6, 16);
+  if  bestSolCost == 176
+    disp('17t6m test PASSED!');
+  else
+    disp('17t6m test FAILED!');
+    disp(bestSolCost);
+  end
 end
 
 function [sT] = findStartTemp(jobs, m)
