d1e0ec2b83dff9c0d92e64e7cd2654f7c906f476
Rui Zhao <oahziur@gmail.com>
2014-07-27 21:02:11 -0400

    Adjust iteration

--- SA_16t6m_test.m
+++ SA_16t6m_test.m
@@ -5,7 +5,7 @@ function [costs] = SA_16t6m_test(iterations)
   J = [58,72,79,43,16,37,7,74,97,44,39,80,65,65,39,96];
   sT = findStartTemp(J, m);
   alpha = 0.85;
-  iterationsAtTemp = 55;
+  iterationsAtTemp = 5;
   [costs, bestSol] = SA(S, J, m, n, iterationsAtTemp, sT, iterations, ...
                         alpha, @cost, @gen_neighbor);
   bestSolCost = cost(bestSol, J, 6, 16);
